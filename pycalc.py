9b337e3bbf5cb1883eea7bc3121634ae68f091e2
Lucas <lukinha_araraquara@hotmail.com>
2018-10-28 18:50:24 -0400

    Created the menu function
    Including a menu for the user see the options

--- pycalc.py
+++ pycalc.py
@@ -1,3 +1,11 @@
+def menu():
+    print("### Menu ###")
+    print("[0] - Exit")
+    print("[1] - Sum")
+    print("[2] - Subtraction")
+    print("[3] - Multiplication")
+    print("[4] - Division")
+
 def calc():
     a = int(input("Value 1: "))
     b = int(input("Value 2: "))
@@ -13,6 +21,7 @@ def calc():
 
 try:
     while (True):
+        menu()
         option = int(input())
         if(option > 0 and option < 5):
             calc()
